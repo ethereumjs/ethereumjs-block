26753490245625ab3cf27f71f1dbce2b8ec9258b
Everton Fraga <ev@ethereum.org>
2019-12-13 10:52:54 -0500

    Adds difficulty bomb delay for Muir Glacier hard fork

--- header.js
+++ header.js
@@ -127,7 +127,13 @@ BlockHeader.prototype.canonicalDifficulty = function (parentBlock) {
     dif = parentDif.add(offset.mul(a))
   }
 
-  if (this._common.hardforkGteHardfork(hardfork, 'constantinople')) {
+  if (this._common.hardforkGteHardfork(hardfork, 'muirGlacier')) {
+    // Istanbul/Berlin difficulty bomb delay (EIP2384)
+    num.isubn(9000000)
+    if (num.ltn(0)) {
+      num = new BN(0)
+    }
+  } else if (this._common.hardforkGteHardfork(hardfork, 'constantinople')) {
     // Constantinople difficulty bomb delay (EIP1234)
     num.isubn(5000000)
     if (num.ltn(0)) {
